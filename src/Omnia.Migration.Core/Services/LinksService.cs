806ea6eb0714bd3327e56a79114e3eb7fcfad013
Thoan Tran <thoan.tran.thi@omniaintranet.com>
2024-11-29 14:02:37 +0700

    Fix my links

--- src/Omnia.Migration.Core/Services/LinksService.cs
+++ src/Omnia.Migration.Core/Services/LinksService.cs
@@ -9,6 +9,8 @@ using System;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Omnia.Migration.Core.Reports;
+using Omnia.Fx.Models.Identities;
+using Omnia.Fx.Models.Queries;
 
 namespace Omnia.Migration.Core.Services
 {
@@ -26,24 +28,33 @@ namespace Omnia.Migration.Core.Services
             MigrationSettings = migrationSettings;
         }
 
-        public async ValueTask<QuickLink> AddOrUpdateMyLinkAsync(G1MyLink link)
+        public async ValueTask<QuickLink> AddOrUpdateMyLinkAsync(G1MyLink link, ItemQueryResult<IResolvedIdentity> Identities)
         {
+
             try
             {
-                var g2Link = LinkMapper.MapSharedLink(link, MigrationSettings.Value.WCMContextSettings, MigrationSettings.Value.ImportMyLinksSettings.IconColor, MigrationSettings.Value.ImportMyLinksSettings.BackgroundColor);
-                var addLinkResult = await MyLinkApiHttpClient.AddOrUpdateMyLinkAsync(g2Link);
-                addLinkResult.EnsureSuccessCode();
 
-                using (var connection = new SqlConnection(MigrationSettings.Value.WorkplaceContextSettings.DatabaseConnectionString))
+                var g2Link = LinkMapper.MapSharedLink(link, MigrationSettings.Value.WCMContextSettings, MigrationSettings.Value.ImportMyLinksSettings.IconColor, MigrationSettings.Value.ImportMyLinksSettings.BackgroundColor);
+                if (g2Link != null)
                 {
-                    await connection.ExecuteAsync(@"
+                    var addLinkResult = await MyLinkApiHttpClient.AddOrUpdateMyLinkAsync(g2Link);
+                    addLinkResult.EnsureSuccessCode();
+                    var Icreadby1 = Omnia.Migration.Core.Mappers.UserMaper.GetSystemPropUserIdentitybyEmail(Identities, link.UserLoginName);
+                    if (Icreadby1 != null)
+                    {
+
+                        using (var connection = new SqlConnection(MigrationSettings.Value.WorkplaceContextSettings.DatabaseConnectionString))
+                        {
+                            await connection.ExecuteAsync(@"
                     Update MyLinks 
                     SET UserLoginName = @UserLoginName
-                    WHERE Id = @Id", new { Id = addLinkResult.Data.Id, UserLoginName = link.UserLoginName });
+                    WHERE Id = @Id", new { Id = addLinkResult.Data.Id, UserLoginName = Icreadby1 });
+                        }
+                    }
+                    ImportLinksReport.Instance.AddSucceedLink(link.Url);
+                    return addLinkResult.Data;
                 }
-
-                ImportLinksReport.Instance.AddSucceedLink(link.Url);
-                return addLinkResult.Data;
+                return null;
             }
             catch (Exception ex)
             {
